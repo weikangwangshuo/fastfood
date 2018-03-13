9596bd6277035cdee6869845f85ebf78472e4449
Administrator <Administrator@2VSZHQZ0X4BMK1W>
2018-03-13 17:56:28 +0800

    多数据源配置有问题

--- src/main/java/com/ws/fastfood/login/Jdbc/MyBatisConfig.java
+++ src/main/java/com/ws/fastfood/login/Jdbc/MyBatisConfig.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -21,9 +20,10 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
+import com.alibaba.druid.pool.xa.DruidXADataSource;
 import com.github.pagehelper.PageHelper;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import com.ws.fastfood.login.Util.PropertiesUtils;
+
 
 /**
  * DataSource、SqlSessionFactory和Transaction Manager 配置
@@ -33,74 +33,63 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 @EnableTransactionManagement
 public class MyBatisConfig implements TransactionManagementConfigurer{
-	private final static Logger logger = LoggerFactory.getLogger(MyBatisConfig.class);
-
-	//数据库连接相关的参数：
-	private String driverClassName = "com.mysql.jdbc.Driver";
-    private String jdbcIpPort="localhost:3306";
-	private String jdbcUrl = "jdbc:mysql://localhost:3306/fastfood?useUnicode=true&characterEncoding=UTF-8";
-	private String userName = "root";
-	private String password = "2008ws";
-	
-	//连接池相关的参数：
-	//等待从连接池中获得连接的最大时长（毫秒），超过这个时长还没可用的连接则发生SQLException， 缺省:30秒
-	private long connectionTimeout = 30000;
-	//一个连接idle状态的最大时长（毫秒），超时则被释放（retired），缺省:10分钟
-	private long idleTimeout = 600000;
-	//一个连接的生命时长（毫秒），超时而且没被使用则被释放（retired），缺省:30分钟，建议设置比数据库超时时长少30秒以上，
-	//参考MySQL wait_timeout参数（show variables like '%timeout%';）
-	private long maxLifetime = 1765000;
-	//连接池中允许的最大连接数。缺省值：10；推荐的公式：((core_count * 2) + effective_spindle_count)
-	private int maximumPoolSize = 15;
-	
-	/**
-	 * 配置dataSource，使用Hikari连接池 
-	 */
-	@Bean(destroyMethod = "close")
-	@Primary
-	public DataSource dataSource1(){
-		HikariConfig config = new HikariConfig();
-		config.setDriverClassName(driverClassName);
-		config.setJdbcUrl(String.format(jdbcUrl, jdbcIpPort, "test"));
-		config.setUsername(userName);
-		config.setPassword(password);
-		config.setConnectionTimeout(connectionTimeout); 
-		config.setIdleTimeout(idleTimeout);
-		config.setMaxLifetime(maxLifetime);
-		config.setMaximumPoolSize(maximumPoolSize);
-		
-		HikariDataSource ds = new HikariDataSource(config);
-		return ds;
-	}
-
-	@Bean(destroyMethod = "close")  
-	public DataSource dataSource2(){
-		HikariConfig config = new HikariConfig();
-		config.setDriverClassName(driverClassName);
-		config.setJdbcUrl(String.format(jdbcUrl, jdbcIpPort, "my_db"));
-		config.setUsername(userName);
-		config.setPassword(password);
-		config.setConnectionTimeout(connectionTimeout);
-		config.setIdleTimeout(idleTimeout);
-		config.setMaxLifetime(maxLifetime);
-		config.setMaximumPoolSize(maximumPoolSize);
-		
-		HikariDataSource ds = new HikariDataSource(config);
-		return ds;
-	}
 
 	/**
-	 * 配置SqlSessionFactory：
-	 * - 创建SqlSessionFactoryBean，并指定一个dataSource；
-	 * - 设置这个分页插件：https://github.com/pagehelper/Mybatis-PageHelper；
-	 * - 指定mapper文件的路径；
-	 */
-	@Bean
+     * 配置dataSource，使用Druid连接池
+     */
+    @Bean(destroyMethod = "close")
+    @Primary
+    public DataSource dataSource1(){
+        DruidXADataSource dataSource = new DruidXADataSource();
+        dataSource.setDriverClassName(PropertiesUtils.findPorpertyByKey("jdbc1.driverClassName","config/datasources1.properties").trim());
+        dataSource.setUrl(PropertiesUtils.findPorpertyByKey("jdbc1.jdbcUrl","config/datasources1.properties").trim());
+        dataSource.setUsername(PropertiesUtils.findPorpertyByKey("jdbc1.userName","config/datasources1.properties").trim());
+        dataSource.setPassword(PropertiesUtils.findPorpertyByKey("jdbc1.password","config/datasources1.properties").trim());
+        dataSource.setInitialSize(5); // 连接池启动时创建的初始化连接数量（默认值为0）
+        dataSource.setMaxActive(20); // 连接池中可同时连接的最大的连接数
+        //dataSource.setMaxIdle(12); // 连接池中最大的空闲的连接数，超过的空闲连接将被释放，如果设置为负数表示不限
+        dataSource.setMinIdle(0); // 连接池中最小的空闲的连接数，低于这个数量会被创建新的连接
+        dataSource.setMaxWait(60000); // 最大等待时间，当没有可用连接时，连接池等待连接释放的最大时间，超过该时间限制会抛出异常，如果设置-1表示无限等待
+        dataSource.setRemoveAbandonedTimeout(180); // 超过时间限制，回收没有用(废弃)的连接
+        dataSource.setRemoveAbandoned(true); // 超过removeAbandonedTimeout时间后，是否进 行没用连接（废弃）的回收
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setTestWhileIdle(true);
+        //dataSource.setValidationQuery("SELECT 1");
+        dataSource.setTimeBetweenEvictionRunsMillis(1000 * 60 * 30); // 检查无效连接的时间间隔 设为30分钟
+        return dataSource;
+    }
+
+
+    /**
+     * 配置dataSource，使用Druid连接池
+     */
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource2(){
+        DruidXADataSource dataSource = new DruidXADataSource();
+        dataSource.setDriverClassName(PropertiesUtils.findPorpertyByKey("jdbc2.driverClassName","config/datasources2.properties").trim());
+        dataSource.setUrl(PropertiesUtils.findPorpertyByKey("jdbc2.jdbcUrl","config/datasources2.properties").trim());
+        dataSource.setUsername(PropertiesUtils.findPorpertyByKey("jdbc2.userName","config/datasources2.properties").trim());
+        dataSource.setPassword(PropertiesUtils.findPorpertyByKey("jdbc2.password","config/datasources2.properties").trim());
+        dataSource.setInitialSize(5); // 连接池启动时创建的初始化连接数量（默认值为0）
+        dataSource.setMaxActive(20); // 连接池中可同时连接的最大的连接数
+        //dataSource.setMaxIdle(12); // 连接池中最大的空闲的连接数，超过的空闲连接将被释放，如果设置为负数表示不限
+        dataSource.setMinIdle(0); // 连接池中最小的空闲的连接数，低于这个数量会被创建新的连接
+        dataSource.setMaxWait(60000); // 最大等待时间，当没有可用连接时，连接池等待连接释放的最大时间，超过该时间限制会抛出异常，如果设置-1表示无限等待
+        dataSource.setRemoveAbandonedTimeout(180); // 超过时间限制，回收没有用(废弃)的连接
+        dataSource.setRemoveAbandoned(true); // 超过removeAbandonedTimeout时间后，是否进 行没用连接（废弃）的回收
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTimeBetweenEvictionRunsMillis(1000 * 60 * 30); // 检查无效连接的时间间隔 设为30分钟
+        return dataSource;
+    }
+
+    @Bean
     public SqlSessionFactory sqlSessionFactory1() {
-		
-		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource1());
-		
+
         //分页插件
         PageHelper pageHelper = new PageHelper();
         Properties properties = new Properties();
@@ -109,24 +98,22 @@ public class MyBatisConfig implements TransactionManagementConfigurer{
         properties.setProperty("pageSizeZero", "true");
         pageHelper.setProperties(properties);
         bean.setPlugins(new Interceptor[]{pageHelper});
-        
         try {
-        	//指定mapper xml目录
-        	ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        	bean.setMapperLocations(resolver.getResources("classpath:Mapper/*.xml"));
-			return bean.getObject();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			throw new RuntimeException(e);
-		}
-	}
-	
-	@Bean
+            //指定mapper xml目录
+            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            bean.setMapperLocations(resolver.getResources("classpath*:Mapper/*.xml"));
+            return bean.getObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
     public SqlSessionFactory sqlSessionFactory2() {
-		
-		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource2());
-		
+
         //分页插件
         PageHelper pageHelper = new PageHelper();
         Properties properties = new Properties();
@@ -135,30 +122,25 @@ public class MyBatisConfig implements TransactionManagementConfigurer{
         properties.setProperty("pageSizeZero", "true");
         pageHelper.setProperties(properties);
         bean.setPlugins(new Interceptor[]{pageHelper});
-        
+
         try {
-        	//指定mapper xml目录
-        	ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        	bean.setMapperLocations(resolver.getResources("classpath:Mapper/*.xml"));
-			return bean.getObject();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			throw new RuntimeException(e);
-		}
-	}
-	
-	
-	/**
-	 * Transaction 相关配置
-	 * 因为有两个数据源，所有使用ChainedTransactionManager把两个DataSourceTransactionManager包括在一起。
-	 */
-	@Override
-	public PlatformTransactionManager annotationDrivenTransactionManager() {
-		DataSourceTransactionManager dtm1 = new DataSourceTransactionManager(dataSource1());
-		DataSourceTransactionManager dtm2 = new DataSourceTransactionManager(dataSource2());
-
-		ChainedTransactionManager ctm = new ChainedTransactionManager(dtm1, dtm2);
-		return ctm;
-	}
-
+            //指定mapper xml目录
+            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            bean.setMapperLocations(resolver.getResources("classpath*:Mapper/*.xml"));
+            return bean.getObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /**
+     * Transaction 相关配置
+     * 因为有两个数据源，所有使用ChainedTransactionManager把两个DataSourceTransactionManager包括在一起。
+     */
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        DataSourceTransactionManager dtm1 = new DataSourceTransactionManager(dataSource1());
+        DataSourceTransactionManager dtm2 = new DataSourceTransactionManager(dataSource2());
+        ChainedTransactionManager ctm = new ChainedTransactionManager(dtm1, dtm2);
+        return ctm;
+    }
 }
